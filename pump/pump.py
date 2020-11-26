b3bebabda3cc1e9949fd58751c5bfef5fc7fd147
erovirosa <emmanuel.Rovirosa@gmail.com>
2020-11-26 00:56:46 +0000

    fixing bugs after first install

--- pump/pump.py
+++ pump/pump.py
@@ -13,50 +13,70 @@ import constants
 DEVICE_ADDR = 0x10
 bus = smbus.SMBus(1)
 
-def pumpOn(relay):
-	bus.write_byte_data(DEVICE_ADDR, relay, 0xFF)
+def pumpOn():
+	bus.write_byte_data(DEVICE_ADDR, 1, 0xFF)
+	bus.write_byte_data(DEVICE_ADDR, 2, 0xFF)
+	bus.write_byte_data(DEVICE_ADDR, 3, 0xFF)
+	bus.write_byte_data(DEVICE_ADDR, 4, 0xFF)
 	print("pump turned on")
 
-def pumpOff(relay):
-	bus.write_byte_data(DEVICE_ADDR, relay, 0x00)
+def pumpOff():
+	bus.write_byte_data(DEVICE_ADDR, 1, 0x00)
+	bus.write_byte_data(DEVICE_ADDR, 2, 0x00)
+	bus.write_byte_data(DEVICE_ADDR, 3, 0x00)
+	bus.write_byte_data(DEVICE_ADDR, 4, 0x00)
 	print("pump turned off")
 
 #def sigHandler(signal_received, frame):
 def sigHandler():
 	print("Pump turned off and exiting program")
-	pumpOff(1)
+	pumpOff()
 	sys.exit(0)
 
-def warmWater(address):
+def warmWater(address, threshold):
 	try:
 		while (True):
-			pumpOn(1)
+			pumpOn()
 			time.sleep(1)
-			masterT = float(requests.get(address).content)
-			print(masterT)
-			if (masterT > 75):
+			temp = float(requests.get(address).content)
+			print(temp)
+			if (temp > threshold):
 				break
 	except:
 		print("Error occured when trying to warm Master Bathroom water")
 	finally:
-		pumpOff(1)
-	return 
+		pumpOff()
+	return
 
 app = Flask(__name__)
 
 @app.route('/Temp')
 def getTemp():
-	masterT = float(requests.get("http://192.168.0.114:8080/temp").content)
+	try:
+		masterT = float(requests.get("http://192.168.0.152:8080/temp").content)
+	except:
+		masterT =  "Sensor Unavailable"
 	print (masterT)
-	#guestT = requests.get("http://123/temp")
-	return render_template("water.html", masterT=masterT)
+	try:
+		guestT = float(requests.get("http://192.168.0.114:8080/temp").content)
+	except:
+		guestT = "Sensor Unavailable"
+	print (guestT)
+	return render_template("water.html", masterT=masterT, guestT=guestT)
 
 @app.route('/warmMaster')
 def warmMaster():
 	print("Master bath set to warm")
-	pump_thread = threading.Thread(target=warmWater, daemon=True, args=("http://192.168.0.114:8080/temp",))
+	pump_thread = threading.Thread(target=warmWater, daemon=True, args=("http://192.168.0.152:8080/temp", 85))
 	pump_thread.start()
 	return redirect('/Temp')
+
+@app.route('/warmGuest')
+def warmGuest():
+        print("Guest bath set to warm")
+        pump_thread = threading.Thread(target=warmWater, daemon=True, args=("http://192.168.0.114:8080/temp", 85))
+        pump_thread.start()
+        return redirect('/Temp')
 
 def main():
 	app.run(host = '0.0.0.0', port=constants.port, debug=True)
--- pump/start.sh
+++ pump/start.sh
@@ -1,4 +1,4 @@
 echo before pip > /home/pi/waterRecirculation/pump/pump.log
-pip3 install -r /home/pi/waterRecirculation/pump/requirements.txt >> /home/pi/waterRecirculation/pump/pump.log 2>&1
+#pip3 install -r /home/pi/waterRecirculation/pump/requirements.txt >> /home/pi/waterRecirculation/pump/pump.log 2>&1
 #pip3 list >> /home/pi/waterRecirculation/pump/pump.log 2>&1
 python3 /home/pi/waterRecirculation/pump/pump.py >> /home/pi/waterRecirculation/pump/pump.log 2>&1 &
