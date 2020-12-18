bb9ca67fcdabb54c2e994a1c84ca158e0a506f0f
erovirosa <emmanuel.rovirosa@gmail.com>
2020-12-18 01:47:13 +0000

    addicng rounding

--- readTemp/start.sh
+++
@@ -1,6 +0,0 @@
-#echo log this > /home/pi/water/readTemp/readTemp.log
-#source /home/pi/water/readTemp/env/bin/activate
-echo before pip > /home/pi/waterRecirculation/readTemp/readTemp.log
-#pip3 install -r /home/pi/water/readTemp/requirements.txt >> /home/pi/water/readTemp/readTemp.log 2>&1
-#pip3 list >> /home/pi/water/readTemp/readTemp.log 2>&1
-python3 /home/pi/waterRecirculation/readTemp/readTemp.py >> /home/pi/waterRecirculation/readTemp/readTemp.log 2>&1 &
